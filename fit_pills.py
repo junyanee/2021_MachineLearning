46667bd6817836a08e751468dbe72ba2cf7766ee
박준현 <grssa@naver.com>
2021-07-12 14:18:33 +0900

    cnn_pills

---
+++ fit_pills.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 11 18:14:31 2021
+
+@author: junyanee
+"""
+
+import os, glob
+from PIL import Image
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+import matplotlib.pyplot as plt
+
+# 데이터셋 위치
+image_dir = 'D:\D:\한이음\pills\'
+
+# 클래스 개수 (아웃풋 개수)
+categories = os.listdir(image_dir)
+classes = len(categories)
+print("pills에는 ", classes, "개의 클래스가 있습니다.")
+
+# 이미지 사이즈 값 설정
+image_width = 32
+image_height = 32
+
+# 어레이 생성
+x = []
+y = []
+
+# 데이터셋 전처리 #
+for idx, cat in enumerate(categories):
+    # 원 핫 코드로 변경
+    label = [0 for i in range(classes)]
+    label[idx] = 1
+    
+    # 이미지 불러오기
+    image_dir_detail = image_dir + "/" + cat
+    files = glob.glob(image_dir_detail + "/*.jpg")
+    print(cat, " 파일 개수 : ", len(files))
+    
+    # 불러온 이미지 전처리해서 배열로 저장
+    for i, f in enumerate(files):
+        img = Image.open(f)
+        img = img.convert("RGB")
+        img = img.resize((image_width, image_height))
+        pixel = np.asarray(img)
+
+        x.append(pixel)
+        y.append(label)
+
+# 전처리한 값 어레이에 넣기
+x = np.array(x)
+y = np.array(y)
+
+# 훈련, 테스트 집합 분리
+x_train, x_test, y_train, y_test = train_test_split(x, y)
+xy = (x_train, x_test, y_train, y_test)
+
+# npy 파일로 저장
+np.save("kFood_kind_image_data_KerasCNN.npy", xy)
+
+# 전처리한 데이터 로드
+x_train, x_test, y_train, y_test = np.load('./kFood_kind_image_data_KerasCNN.npy', allow_pickle = True)
+
+# 텐서구조 출력
+print(x_train.shape, y_train.shape)
+
+# 0~1 사이의 값으로 정규화
+x_train = x_train.astype(np.float32) / 255.0
+x_test = x_test.astype(np.float32) / 255.0
+
+# 제작한 cnn model : C-C-P-D - C-P-D - C-P-D - FC-D-FC-FC-FC
+
+# 신경망 모델 #
+cnn=Sequential()
+cnn.add(Conv2D(32, (3, 3), activation = 'relu', input_shape = (image_width, image_height, 3)))
+cnn.add(Conv2D(32, (3, 3), activation = 'relu'))
+cnn.add(MaxPooling2D(pool_size = (2, 2)))
+cnn.add(Dropout(0.25))
+
+cnn.add(Conv2D(64, (3, 3), activation = 'relu'))
+cnn.add(MaxPooling2D(pool_size = (2, 2)))
+cnn.add(Dropout(0.25))
+
+cnn.add(Conv2D(64, (3, 3), activation = 'relu'))
+cnn.add(MaxPooling2D(pool_size = (2, 2)))
+cnn.add(Dropout(0.25))
+
+cnn.add(Flatten())
+cnn.add(Dense(256, activation = 'relu'))
+cnn.add(Dropout(0.5))
+cnn.add(Dense(32, activation = 'relu'))
+cnn.add(Dense(16, activation = 'relu'))
+cnn.add(Dense(classes, activation = 'softmax'))
+
+
+# 모델 구조 확인
+cnn.summary()
+
+# 신경망 모델 학습
+cnn.compile(loss = 'categorical_crossentropy', optimizer = Adam(), metrics = ['accuracy'])
+hist = cnn.fit(x_train, y_train, batch_size = 32, epochs = 100, validation_data = (x_test, y_test), verbose = 1)
+
+# 신경망 구조, 가중치 저장
+cnn.save("kFood_cnn_KerasCNN_2.h5")
+
+# 신경망 모델 정확률 평가
+res = cnn.evaluate(x_test, y_test, verbose = 0)
+print("accuracy is", res[1] * 100)
+
+# 정확률 그래프
+plt.plot(hist.history['accuracy'])
+plt.plot(hist.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc = 'best')
+plt.grid()
+plt.show()
+
+# 손실함수 그래프
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc = 'best')
+plt.grid()
+plt.show()
---
+++ predict_pills.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+@author: junyanee
+
+새로운 영상 예측
+"""
+
+import os
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+import matplotlib.image as img
+import matplotlib.pyplot as plt
+
+# 이미지 전처리하는 함수
+def Dataization(img_path):
+    image_w = 32
+    image_h = 32
+    img = cv2.imread(img_path)
+    img = cv2.resize(img, None, fx=image_w/img.shape[1], fy=image_h/img.shape[0])
+    return (img/256)
+
+image_dir= './test_kFood'
+
+# 출력층 클래스
+categories = ['센트롬','오메가3','타이레놀','루테인']
+
+src = []
+name = []
+test = []
+
+# 경로 돌면서 이미지 전처리
+for file in os.listdir(image_dir):
+    if (file.find('.jpg') is not -1):
+        src.append(image_dir +"/"+ file)
+        name.append(file)
+        test.append(Dataization(image_dir +"/"+ file))
+        
+        # 이미지 보여주기
+        ndarray = img.imread(image_dir +"/"+ file)
+        plt.title(file)
+        plt.imshow(ndarray)
+        plt.show()
+        
+    
+
+# 저장된 모델 가져와서 예측
+test = np.array(test)
+model = load_model('kFood_cnn_KerasCNN_2.h5')
+predict = model.predict_classes(test)
+
+for i in range(len(test)):
+    print(name[i], "사진은", (categories[predict[i]]), "(으)로 판별됩니다.")
+
